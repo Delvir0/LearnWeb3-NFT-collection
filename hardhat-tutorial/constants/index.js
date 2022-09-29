0d260065d4965d2cb386821a30c04e2aabfdd2b2
Delvir0 <delvir0x0@gmail.com>
2022-09-29 14:27:13 +0200

    first message

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xc7cc66EAb35ff0ac4Ed8b2C30415978AffdbB048";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
